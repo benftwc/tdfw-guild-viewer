2bc5588ec58ad853a750f8299eebe9cfdd3c7687
CASSINAT Benjamin <benjamin@pldev.fr>
2017-07-11 01:54:54 +0200

    Add Skeleton

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,27 +1,27 @@
 <?php
 
 //Routes of the application.
-\Flight::route('/', array('Tdfw\GuildViewer\Controller\GuifdsldViewer', 'index'));
+\Flight::route('/', array('\Acme\Demo\Controller\Demo', 'index'));
 
 //Mappings.
 \Flight::map('notFound', function() {
-    \Flight::render('404', array());
+	\Flight::render('404', array());
 });
 
 \Flight::map('error', function(\Exception $e) {
-    //You probably want to log the error.
-    switch($e->getCode()) {
-        case E_ERROR:
-        case E_USER_ERROR:
-        case E_CORE_ERROR:
-            break;
-        case E_WARNING:
-        case E_USER_WARNING:
-        case E_CORE_WARNING:
-            break;
-        case E_NOTICE:
-        case E_USER_NOTICE:
-            break;
-        default:
-    }
+	//You probably want to log the error.
+	switch($e->getCode()) {
+		case E_ERROR:
+		case E_USER_ERROR:
+		case E_CORE_ERROR:
+			break;
+		case E_WARNING:
+		case E_USER_WARNING:
+		case E_CORE_WARNING:
+			break;
+		case E_NOTICE:
+		case E_USER_NOTICE:
+			break;
+		default:
+	}
 });
--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/config/config.php';
 require __DIR__ . '/../app/config/routes.php';
