ba6bb4e3380974bbb47b44ca9411240e8a01dc36
CASSINAT Benjamin <benjamin@pldev.fr>
2017-07-11 00:54:50 +0200

    Example de call

--- inc/core.php
+++ inc/core.php
@@ -1,9 +1,10 @@
 <?php
 
 function getPageRenderer() {
-//  return getTreasury("9E4E5390-6048-E711-80D3-E4115BD7186D");
-//  return getUpgrades("229");
-//  return getItemDetails(2322);
+  //  return getTreasury("9E4E5390-6048-E711-80D3-E4115BD7186D");
+  //  return getUpgrades("229");
+  //  return getItemDetails(2322);
+  parseItemList([]);
 }
 
 function getTreasury($guildId) {
@@ -18,7 +19,13 @@ function getUpgrades($upgradeId) {
   return callAPI("guild/upgrades/$upgradeId");
 }
 
-function callAPI($endpoint) {
+function parseItemList($item_ids) {
+  $guilde = "9E4E5390-6048-E711-80D3-E4115BD7186D";
+  $treasury = json_decode(getTreasury($guilde));
+  var_dump($treasury);
+}
+
+function callAPI($endpoint, $debug = FALSE) {
   $arena_net_api = 'https://api.guildwars2.com/v2';
   $guild_token = '9525EA30-7A1F-5A4E-B94D-440F4FAA13D0D4863785-5C94-46DA-826C-4E7C2BEC26FB';
   $client = new \GuzzleHttp\Client();
@@ -28,11 +35,11 @@ function callAPI($endpoint) {
         'Authorization' => "Bearer {$guild_token}",
       ],
   ]);
-  echo $res->getStatusCode();
-  // 200
-  echo $res->getHeaderLine('content-type');
-  // 'application/json; charset=utf8'
-  echo $res->getBody();
-  // '{"id": 1420053, "name": "guzzle", ...}'
 
+  if ($debug) {
+    return $res;
+  }
+  else {
+    return $res->getBody();
+  }
 }
