d4ec1b43e5d77d1bf47685fb588a11471d5f2d1f
CASSINAT Benjamin <benjamin@pldev.fr>
2017-07-11 00:02:05 +0200

    Add index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'vendor/autoload.php';
+
+Flight::route('/', function () {
+  echo 'hello world!';
+});
+
+
+
+
+
+
+
+
+Flight::set('flight.base_url ', NULL);        // Override the base url of the request . (default: NULL)
+Flight::set('flight.case_sensitive ', FALSE);  // Case sensitive matching for URLs . (default: FALSE)
+Flight::set('flight.handle_errors ', TRUE);   // Allow Flight to handle all errors internally . (default: TRUE)
+Flight::set('flight.log_errors ', FALSE);      // Log errors to the web server's error log file. (default: false)
+Flight::set('flight.views.path', "./views");       // Directory containing view template files. (default: ./views)
+Flight::set('flight.views.extension', ".php");  // View template file extension. (default: .php)
+
+
+Flight::start();
