9c4460d5d95d6a1a85ae0a5aeb76b67be71a9fd2
CASSINAT Benjamin <benjamin@pldev.fr>
2017-07-11 02:04:17 +0200

    Add configs

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,10 @@
 <?php
 
 //Configure your application.
-
+Flight::set('tdfw.guild_id', "9E4E5390-6048-E711-80D3-E4115BD7186D");
+Flight::set('tdfw.guild_name', "Turn Down For Wipe");
+Flight::set('tdfw.api_token', "9525EA30-7A1F-5A4E-B94D-440F4FAA13D0D4863785-5C94-46DA-826C-4E7C2BEC26FB");
+Flight::set('tdfw.api_endpoint', "https://api.guildwars2.com/v2");
 
 //Settings for Flight.
 \Flight::set('flight.views.path', __DIR__ . '/../resources/templates');
