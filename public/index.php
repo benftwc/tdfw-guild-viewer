55559504fafbdb0ac8070bf39a88026627990b72
Sullivan Bethouart <sullivan@bethouart.fr>
2017-07-14 13:46:51 +0200

    Add SASS on project then integrate treasury page

--- public/index.php
+++ public/index.php
@@ -3,4 +3,8 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/config/config.php';
 require __DIR__ . '/../app/config/routes.php';
 
+if (getenv('APPLICATION_ENV') == 'development') {
+    SassCompiler::run("../app/resources/scss/", "css/");
+}
+
 \Flight::start();
